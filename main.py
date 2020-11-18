334a4e1ec8005fe7ca4f349cce3aebaf1e910e6f
yonatan245 <yonatan245@gmail.com>
2020-11-18 21:31:02 +0200

    need to check how to get the indexes of max of matrix

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 import argparse as arg
 
-threshold = 120
+lines_counter = 0
+circles_counter = 0
 
 
 def get_point_lines(x, y):
@@ -13,25 +14,74 @@ def get_point_lines(x, y):
 
 
 def draw_line_by_x(m, c):
-    for x in range(len(img)):
+    (x_start, x_end, y_start, y_end) = -1, -1, -1, -1
+    for x in range(len(img)-1, -1, -1):
+        y = np.floor(m * x + c)
+
+        if y != np.NaN and 0 <= y < len(img[0]) and R_img[x, int(y)] == 255:
+            x_end = x
+            y_end = int(y)
+            break
+
+    for x in range(0, x_end):
         y = np.floor(m * x + c)
 
         if y != np.NaN and 0 <= y < len(img[0]):
-            img[x, int(y)] = 255
+            if R_img[x, int(y)] == 255 or x_start != -1:
+                img[x, int(y)] = 255
+                if x_start == -1:
+                    x_start = x
+                    y_start = int(y)
+
+    if x_start != -1:
+        output.write(f'{x_start} {y_start} {x_end} {y_end}\n')
 
 
 def draw_line_by_y(m, c):
-    for y in range(len(img[0])):
-        x = np.floor((c - y) / m)
+    (x_start, x_end, y_start, y_end) = -1, -1, -1, -1
+    for y in range(len(img[0]) - 1, -1, -1):
+        x = np.floor((c-y)/m)
+
+        if x != np.NaN and 0 <= x < len(img) and R_img[int(x), y] == 255:
+            x_end = int(x)
+            y_end = y
+            break
+
+    for y in range(0, y_end):
+        x = np.floor((c-y)/m)
 
         if x != np.NaN and 0 <= x < len(img):
-            img[int(x), y] = 255
+            if R_img[int(x), y] == 255 or y_start != -1:
+                img[int(x), y] = 255
+                if y_start == -1:
+                    x_start = int(x)
+                    y_start = y
+    if y_start != -1:
+        output.write(f'{x_start} {y_start} {x_end} {y_end}\n')
 
 
 def draw_line(r, theta):
     if theta == 0:
-        for y in range(len(img[0])):
-            img[r, y] = 255
+        (x_start, x_end, y_start, y_end) = -1, -1, -1, -1
+        for y in range(len(img[0]) - 1, -1, -1):
+            x = r
+
+            if x != np.NaN and 0 <= x < len(img) and R_img[int(x), y] == 255:
+                x_end = int(x)
+                y_end = y
+                break
+
+        for y in range(0, y_end):
+            x = r
+
+            if x != np.NaN and 0 <= x < len(img):
+                if R_img[int(x), y] == 255 or y_start != -1:
+                    img[int(x), y] = 255
+                    if y_start == -1:
+                        x_start = int(x)
+                        y_start = y
+        if y_start != -1:
+            output.write(f'{x_start} {y_start} {x_end} {y_end}\n')
 
     else:
         theta_rad = np.radians(theta)
@@ -56,9 +106,11 @@ if __name__ == '__main__':
 
     img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2GRAY)
     R_img = cv2.Canny(img, 100, 200)
+    output = open(output_path, "w+")
 
     max_r = int(np.ceil(np.sqrt(np.power(len(img), 2) + np.power(len(img[0]), 2))))
     lines_matrix = np.zeros((max_r, 360))
+    lines_counter = 0
 
     cv2.imwrite('Response image.png', R_img)
 
@@ -69,9 +121,21 @@ if __name__ == '__main__':
 
     cv2.imwrite('lines matrix.png', lines_matrix)
 
-    for r in range(max_r):
-        for theta in range(360):
-            if lines_matrix[r, theta] >= threshold:
-                draw_line(r, theta)
+    threshold = lines_matrix.max() * 0.6
+
+    local_x, local_y = np.argmax(lines_matrix)
+    local_value = lines_matrix[local_x, local_y]
+    while local_value >= threshold:
+        draw_line(local_x,local_y)
+        lines_counter += 1
+        local_x, local_y = np.argmax(lines_matrix)
+        local_value = lines_matrix[local_x, local_y]
+
+    # for r in range(max_r):
+    #     for theta in range(360):
+    #         if lines_matrix[r, theta] >= threshold:
+    #             draw_line(r, theta)
+    #             lines_counter += 1
 
     cv2.imwrite('pic_with_lines.png', img)
+    output.close()
